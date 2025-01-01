11bd9a13f921525ef913d7159a023298332a7c53
Alex Cuadra <alexcu21@live.com>
2025-01-01 17:17:13 -0600

    adding cloudflare support

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,13 @@
 import { defineConfig } from 'astro/config';
 import dotenv from 'dotenv'
 import tailwind from '@astrojs/tailwind';
+import cloudflare from '@astrojs/cloudflare';
 
 dotenv.config()
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  output: 'server',
+  adapter: cloudflare(),
 });
